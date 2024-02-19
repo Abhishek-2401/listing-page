0478d5aa8bd20b55bae726bd156476290042f17b
Abhishek-2401 <158551171+Abhishek-2401@users.noreply.github.com>
2024-02-19 18:25:13 +0530

    Update group2214.jsx

--- src/components/group2214.jsx
+++ src/components/group2214.jsx
@@ -1,10 +1,8 @@
-
 import React from "react";
 import Packages from '../components/packages.jsx'
-
 import "../styles.css";
 
-const group2214 = () => {
+const Group2214 = () => {
   return (
     <div className="">
       <div
@@ -14,21 +12,17 @@ const group2214 = () => {
         
         <div className="absolute inset-0 bg-gradient-to-r from-black to-transparent opacity-50"></div>
 
-        <div className="text-content absolute inset-0 flex items-center justify-between p-8">
-          <div className="text-white ml-12 mb-24">
-            <h1 className="text-3xl font-bold ">Munnar</h1>
-            <p className="text-lg font-normal text0-base">
+        <div className="text-content absolute inset-0 flex items-center justify-between p-4 sm:p-8">
+          <div className="text-white ml-4 sm:ml-12 mb-8 sm:mb-24">
+            <h1 className="text-xl sm:text-3xl font-bold">Munnar</h1>
+            <p className="text-base sm:text-lg font-normal">
               Plan your dream trip with campfly
             </p>
           </div>
         </div>
       </div>
-      
-      
-      
     </div>
-  
   );
 };
 
-export default group2214;
+export default Group2214;
