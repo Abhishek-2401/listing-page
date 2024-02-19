3dc971d988f38757d354babb8d1d8b999c70f86c
Abhishek-2401 <158551171+Abhishek-2401@users.noreply.github.com>
2024-02-19 18:26:02 +0530

    Update packages.jsx

--- src/components/packages.jsx
+++ src/components/packages.jsx
@@ -1,25 +1,21 @@
-
 import React from 'react';
 import Filters from './filters';
-import PackageCard from './packagecard'; 
+import PackageCard from './packagecard';
 import '../styles.css';
-
 
 const PackagesComponent = () => {
   return (
-    <div className="absolute z-10 packages-container top-[45%] w-11/12 h-fit ml-20 right-16 bg-white flex">
-    
-      <div className="flex flex-col">
-        
-        <div className="flex justify-start items-center bg-white p-4 border-b-2 border-gray-300 rounded-tl-3xl rounded-tr-3xl">
-          
-          <div className="flex items-center space-x-60">
-            <h3 className="text-lg font-semibold ml-10">Filters</h3>
-            <button className="text-sm text-blue-500 hover:underline">Reset All</button>
+    <div className="absolute z-10 packages-container top-[45%] w-11/12 h-fit ml-20 right-16 lg:right-16 md:right-10 md:ml-20 sm:right-10 sm:ml-20 bg-white flex flex-col sm:flex-row">
+      <div className="flex flex-col w-full">
+        <div className="flex justify-start items-center p-4 border-b-2 border-gray-300 rounded-tl-3xl rounded-tr-3xl">
+          <div className="flex flex-col sm:flex-row items-center sm:items-start sm:space-x-20 md:space-x-60 sm:space-y-0">
+            <h3 className="text-lg font-semibold sm:ml-10">Filters</h3>
+            <button className="text-sm text-blue-500 hover:underline sm:ml-0 sm:mt-0 mt-2">
+              Reset All
+            </button>
           </div>
-          
-          
-          <div className="flex space-x-24 justify-start ml-20">
+
+          <div className="flex flex-col sm:flex-row sm:space-x-8 md:space-x-16 sm:ml-4 md:ml-20 ml-20 mt-2 sm:mt-0">
             <button className="text-sm text-gray-600 hover:text-black hover:font-bold relative">
               All Packages <span className="text-[#787878] text-xs">(12)</span>
             </button>
@@ -35,26 +31,18 @@ const PackagesComponent = () => {
           </div>
         </div>
 
-       
-        <div className="flex ">
-       
+        <div className="flex">
           <Filters />
 
-          <div className="border-r-2 ml-14 border-[#C8C8C8]"></div>
-          <div className="w-8"></div>
+          <div className="border-r-2 ml-4 sm:ml-14 border-[#C8C8C8]"></div>
+          <div className="w-4 sm:w-8"></div>
 
-         
           <div className="flex flex-wrap">
-           
             <PackageCard />
           </div>
         </div>
-
       </div>
-     </div>
-     
-    
+    </div>
   );
 };
-
 export default PackagesComponent;
